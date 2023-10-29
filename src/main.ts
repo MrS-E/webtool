42a6b8b9879482f6e08494cf39637123e6bc492d
MrS-E <simeon.stix@mx.ch>
2023-10-29 16:29:43 +0100

    Revert "changing from prisma to mongoose"
    This reverts commit 5dddca2acd8acd990214d31ad3b3adeeb07e5178.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  mongoose.set('strictQuery', false);
-  await mongoose.connect(process.env.DATABASE_URL);
   await app.listen(3000);
 }
 bootstrap();

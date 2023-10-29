d8ead316136afcab5ca3c5d5524a020d74484469
MrS-E <simeon.stix@mx.ch>
2023-10-29 16:47:50 +0100

    add createToken to auth handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import CreateTokenDTO from './dto/CreateTokenDTO';
+import { PrismaClient, Token, User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { Response } from 'express';
 
+const db: PrismaClient = new PrismaClient();
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post()
+  async createToken(
+    @Body() createToken: CreateTokenDTO,
+    @Res() res: Response,
+  ): Promise<Token | number> {
+    const user: User = await db.user.findUnique({
+      where: { email: createToken.email },
+    });
+
+    if (
+      await bcrypt.compare(
+        user.auth,
+        await bcrypt.hash(createToken.password, 10),
+      )
+    ) {
+      return await db.token.create({ data: { author: user } });
+    }
+    res.send(401);
+  }
+}
---
+++ src/auth/dto/CreateTokenDTO.ts
@@ -0,0 +1,4 @@
+export default class {
+  readonly email: string;
+  readonly password: string;
+}

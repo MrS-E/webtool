d2f78a49ff21108d5c90c5c8b895eae148a20be0
MrS-E <simeon.stix@mx.ch>
2023-10-29 15:24:39 +0100

    changing db from sqlite to mongodb

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-import { sha512} from "js-sha512";
-import bcrypt  from "bcryptjs";
+import { PrismaClient } from '@prisma/client';
+import { sha512 } from 'js-sha512';
+import bcrypt from 'bcrypt';
 
 const db = new PrismaClient();
 
-
-async function seed()  {
+async function seed() {
   await Promise.all([
-      ...await getUsers().then(res => res.map((user) => db.user.create({ data: user }))),
-      ...getModules().map(module=> db.module.create({data: module}))
-    ]
-  );
+    ...(await getUsers().then((res) =>
+      res.map((user) => db.user.create({ data: user })),
+    )),
+  ]);
 }
 
 seed();
@@ -18,31 +17,22 @@ seed();
 async function getUsers() {
   return [
     {
-      firstname: "Hans",
-      lastname: "Meier",
-      email: "hans.meier@gmail.com",
-      auth: await generateHash("Passwort1234")
+      firstname: 'Hans',
+      lastname: 'Meier',
+      email: 'hans.meier@gmail.com',
+      auth: await generateHash('Passwort1234'),
     },
     {
-      firstname: "a",
-      lastname: "k",
-      email: "a.k@gmail.co",
-      auth: await generateHash("123")
-    }
-  ]
+      firstname: 'a',
+      lastname: 'k',
+      email: 'a.k@gmail.co',
+      auth: await generateHash('123'),
+    },
+  ];
 }
 
-function getModules(){
-  return[
-    {
-      module_number: 1,
-      module_name: "Password Manager"
-    }
-  ]
-}
-
-function generateHash(password: string)  {
-  return Promise.resolve(bcrypt
-    .hash(sha512(password), 10)
-    .then(hash=>hash))
+function generateHash(password: string) {
+  return Promise.resolve(
+    bcrypt.hash(sha512(password), 10).then((hash) => hash),
+  );
 }

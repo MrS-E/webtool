35bdb7e21aa9f6982af1e5f04f2631e156069d88
MrS-E <simeon.stix@gmx.ch>
2023-11-08 14:18:49 +0100

    fix bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient, Token, User } from '@prisma/client';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import {Response} from 'express';
 import CreateTokenDTO from "./dto/CreateTokenDTO";
 

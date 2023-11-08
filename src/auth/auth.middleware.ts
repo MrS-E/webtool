fbc2fa66eaf161d5d974fbe974dd3a1655d23ba6
MrS-E <simeon.stix@gmx.ch>
2023-11-08 22:55:54 +0100

    implement authorized checker middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,8 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    next();
+    const id = req.headers.authToken
+    new AuthService().checkToken(id)
+      .then((auth: boolean)=>{
+        if(auth) next()
+        res.send(401)
+      })
+      .catch(()=>res.send(401))
   }
 }

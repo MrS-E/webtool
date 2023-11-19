cfe5bb64ff0f4ee44bdc2678e6903dd36382a4d8
MrS-E <simeon.stix@gmx.ch>
2023-11-19 15:05:53 +0100

    blocking submit in form

--- webtool-ui/src/components/Form.tsx
+++ webtool-ui/src/components/Form.tsx
@@ -1,7 +1,14 @@
 
 function Form(props:any):JSX.Element {
+
+    function mySubmitFunction(e: { preventDefault: () => void; }) {
+        e.preventDefault();
+        props.action()
+        return false;
+    }
+
     return (
-        <form onSubmit={props.action}>
+        <form onSubmit={mySubmitFunction}>
             {props.children}
             <button type={"submit"} disabled={props.active?!props.active:false}>{props.button}</button>
         </form>

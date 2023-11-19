a66ae555a4a54cce67804fea5f58407aa0594b6d
MrS-E <simeon.stix@gmx.ch>
2023-11-19 15:46:48 +0100

    add trycatch function to reduce repeating code

--- webtool-be/src/passwords/passwords.controller.ts
+++ webtool-be/src/passwords/passwords.controller.ts
@@ -17,11 +17,9 @@ import UpdatePasswordsDTO from "./dto/UpdatePasswordsDTO";
 export class PasswordsController {
   constructor(private readonly passwortService: PasswordsService) {}
 
-  @Get()
-  async getAll(@Req() req: Request): Promise<Password[]|number> {
+  async trycatch(func: Function,) :Promise<any> {
     try {
-      const token: string = <string>req.headers["authentication-token"]
-      return await this.passwortService.getAll(token)
+      return await func()
     } catch (e) {
       throw new HttpException({
         status: e.status,
@@ -29,62 +27,35 @@ export class PasswordsController {
         cause: e.error
       });
     }
+  }
+
+  @Get()
+  async getAll(@Req() req: Request): Promise<Password[]> {
+    const token: string = <string>req.headers["authentication-token"]
+    return await this.trycatch(async () => await this.passwortService.getAll(token))
   }
 
   @Get(':id')
   async get(@Param('id') id: string, @Req() req: Request):Promise<Password> {
-    try {
-      const token: string = <string>req.headers["authentication-token"]
-      return await this.passwortService.get(token, id)
-    } catch (e) {
-      throw new HttpException({
-        status: e.status,
-      }, e.status, {
-        cause: e.error
-      });
-    }
+    const token: string = <string>req.headers["authentication-token"]
+    return await this.trycatch(async () => await this.passwortService.get(token, id))
   }
 
   @Post()
   async create(@Body() createPasswordsDTO: CreatePasswordsDTO, @Req() req: Request): Promise<HttpStatus> {
-    try {
-      const token: string = <string>req.headers["authentication-token"]
-      return await this.passwortService.create(token, createPasswordsDTO)
-    } catch (e) {
-      throw new HttpException({
-        status: e.status,
-      }, e.status, {
-        cause: e.error
-      });
-    }
+    const token: string = <string>req.headers["authentication-token"]
+    return await this.trycatch(async () => await this.passwortService.create(token, createPasswordsDTO))
   }
 
   @Put()
   async update(@Body() updatePasswordDTO: UpdatePasswordsDTO, @Req() req: Request):Promise<HttpStatus> {
-    try {
-      const token: string = <string>req.headers["authentication-token"]
-      return await this.passwortService.update(token, updatePasswordDTO)
-    }catch (e) {
-      throw new HttpException({
-        status: e.status,
-      }, e.status, {
-        cause: e.error
-      });
-    }
+    const token: string = <string>req.headers["authentication-token"]
+    return await this.trycatch(async () => await this.passwortService.update(token, updatePasswordDTO))
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string, @Req() req: Request):Promise<HttpStatus> {
-    try {
-      const token: string = <string>req.headers["authentication-token"]
-      return await this.passwortService.delete(token, id)
-    }catch (e) {
-      throw new HttpException({
-        status: e.status,
-      }, e.status, {
-        cause: e.error
-      });
+    const token: string = <string>req.headers["authentication-token"]
+    return await this.trycatch(async () => await this.passwortService.delete(token, id))
     }
-
-  }
 }

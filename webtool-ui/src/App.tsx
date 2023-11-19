aa5266e38206ecfc0852bb1c65e28f4e982168b9
MrS-E <simeon.stix@gmx.ch>
2023-11-19 02:01:53 +0100

    add navigation

--- webtool-ui/src/App.tsx
+++ webtool-ui/src/App.tsx
@@ -1,9 +1,18 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navigation from "./components/Navigation.tsx";
+import Login from "./sites/Login.tsx";
 
 
-function App() {
+function App() : JSX.Element {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Navigation/>}>
+            <Route path={"/login"} element={<Login/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

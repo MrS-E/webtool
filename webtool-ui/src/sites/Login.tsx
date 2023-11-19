5d36985289e2a1dd2e749a69d52b919ba84f32e8
MrS-E <simeon.stix@gmx.ch>
2023-11-19 14:19:18 +0100

    add navigation to main page

--- webtool-ui/src/sites/Login.tsx
+++ webtool-ui/src/sites/Login.tsx
@@ -3,27 +3,36 @@ import Input from "../components/FormInput.tsx";
 import {useState} from "react";
 import fetch from "../classes/fetch.ts";
 import {useCookies} from "react-cookie";
+import {useNavigate} from "react-router-dom";
 
 
 function Login () :JSX.Element  {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
     const [, setCookie] = useCookies(["token"]);
+    const [active, setActive] = useState(true)
 
     const handleSubmit=()=> {
         const builder = new fetch.Builder("/auth")
         builder.setBody({email:email, password:password})
         builder.setMethode("POST")
 
-        builder.build().json()
-            .then(res=> setCookie("token", res, {path: "/"}))
-            .catch(e=>console.error(e))
+        setActive(false)
 
+        builder.build().json()
+            .then(res=> {
+                setCookie("token", res, {path: "/"})
+                useNavigate()("/")
+            })
+            .catch(e=>console.error(e))
+            .finally(()=>{
+                setActive(true)
+            })
     }
 
     return (
         <>
-            <Form button={"Anmelden"} action={handleSubmit}>
+            <Form button={"Anmelden"} action={handleSubmit} active={active}>
                 <Input type={"text"} name={"email"} label={"E-Mail"} value={setEmail}/>
                 <Input type={"password"} name={"password"} label={"Passwort"} value={setPassword}/>
             </Form>

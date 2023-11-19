a14a2a62621d7d76dd57b1f5731ee9e500f58f89
MrS-E <simeon.stix@gmx.ch>
2023-11-19 02:18:35 +0100

    add fetch hook

---
+++ webtool-ui/src/hooks/useFetch.ts
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+
+function useFetch(url:string, methode : any|null|undefined, header:any|null|undefined, body:any|null|undefined){
+    const [data, setData] = useState<any|null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<any|null>(null)
+
+    useEffect(()=>{
+        setLoading(true)
+
+        const abort = new AbortController();
+        const signal = abort.signal;
+
+        fetch(encodeURI(url), {
+            signal,
+            method: methode?methode:"GET",
+            headers: header?header:undefined,
+            body : body?JSON.stringify(body):undefined
+        })
+            .then(res => res.json())
+            .then(res => setData(res))
+            .catch(e => setError(e))
+            .finally(()=>setLoading(false))
+
+        return () => abort.abort();
+    }, [url])
+
+    return {loading, data, error}
+
+}
+
+export default useFetch

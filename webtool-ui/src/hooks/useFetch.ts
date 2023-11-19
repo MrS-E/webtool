1c1e9e72433ad949f86519f82557045d1be8fe8b
MrS-E <simeon.stix@gmx.ch>
2023-11-19 02:27:26 +0100

    add execute condition to hook

--- webtool-ui/src/hooks/useFetch.ts
+++ webtool-ui/src/hooks/useFetch.ts
@@ -1,29 +1,31 @@
 import {useEffect, useState} from "react";
 
-function useFetch(url:string, methode : any|null|undefined, header:any|null|undefined, body:any|null|undefined){
+function useFetch(url:string, methode : any|null|undefined, header:any|null|undefined, body:any|null|undefined, exec: boolean){
     const [data, setData] = useState<any|null>(null)
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<any|null>(null)
 
     useEffect(()=>{
-        setLoading(true)
+        if(exec) {
+            setLoading(true)
 
-        const abort = new AbortController();
-        const signal = abort.signal;
+            const abort = new AbortController();
+            const signal = abort.signal;
 
-        fetch(encodeURI(url), {
-            signal,
-            method: methode?methode:"GET",
-            headers: header?header:undefined,
-            body : body?JSON.stringify(body):undefined
-        })
-            .then(res => res.json())
-            .then(res => setData(res))
-            .catch(e => setError(e))
-            .finally(()=>setLoading(false))
+            fetch(encodeURI(url), {
+                signal,
+                method: methode ? methode : "GET",
+                headers: header ? header : undefined,
+                body: body ? JSON.stringify(body) : undefined
+            })
+                .then(res => res.json())
+                .then(res => setData(res))
+                .catch(e => setError(e))
+                .finally(() => setLoading(false))
 
-        return () => abort.abort();
-    }, [url])
+            return () => abort.abort();
+        }
+    }, [url, exec])
 
     return {loading, data, error}
 

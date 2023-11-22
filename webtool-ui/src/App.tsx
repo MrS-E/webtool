c685a4c997869d9e379ef94a4117b8019725f896
MrS-E <simeon.stix@gmx.ch>
2023-11-22 16:40:58 +0100

    add routes

--- webtool-ui/src/App.tsx
+++ webtool-ui/src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navigation from "./components/Navigation.tsx";
 import Login from "./sites/Login.tsx";
 import Index from "./sites/Index.tsx";
+import Register from "./sites/Register.tsx";
 
 
 function App() : JSX.Element {
@@ -12,6 +13,7 @@ function App() : JSX.Element {
           <Route path={"/"} element={<Navigation/>}>
             <Route index element={<Index/>}/>
             <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/registrieren"} element={<Register/>}/>
           </Route>
         </Routes>
       </BrowserRouter>

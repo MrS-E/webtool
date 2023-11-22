c71703dad17ded6a1d3f74a1fe81c4eac545fd05
MrS-E <simeon.stix@gmx.ch>
2023-11-22 16:40:15 +0100

    set main and home to index

--- webtool-ui/src/sites/Index.tsx
+++ webtool-ui/src/sites/Index.tsx
@@ -1,13 +1,17 @@
 import {useCookies} from "react-cookie";
+import Main from "./Main.tsx";
+import Home from "./Home.tsx";
 
-function Index() {
+function Index() : JSX.Element {
     const [cookies, ] = useCookies(["token"]);
 
     console.log("cookies:", cookies.token)
 
-    return (
-        <></>
-    );
+    if(cookies.token) {
+        return <Main/>;
+    } else {
+        return <Home/>;
+    }
 }
 
 export default Index;

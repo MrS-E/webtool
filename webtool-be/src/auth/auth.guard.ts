42c8ff36752adf81af0ea49d6a86864d54f6950a
MrS-E <simeon.stix@gmx.ch>
2023-11-29 19:04:31 +0100

    simplifications

--- webtool-be/src/auth/auth.guard.ts
+++ webtool-be/src/auth/auth.guard.ts
@@ -9,28 +9,21 @@ export class AuthGuard implements CanActivate {
       context: ExecutionContext,
   ): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    const token = request.headers.authorization
     if (!token) {
       throw new UnauthorizedException();
     }
     try {
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
-      request['user'] = await this.jwtService.verifyAsync(
+      const user = this.jwtService.verify(
           token,
           {
             secret: jwtConstants.secret
           }
-      );
+      )
+      request['user'] = user
     } catch {
       throw new UnauthorizedException();
     }
     return true;
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    //@ts-ignore
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }

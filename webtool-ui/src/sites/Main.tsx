d4b93000a3856fddea14a2964776fc00802db88b
MrS-E <simeon.stix@gmx.ch>
2023-11-29 19:02:04 +0100

    implement dashboard

--- webtool-ui/src/sites/Main.tsx
+++ webtool-ui/src/sites/Main.tsx
@@ -1,5 +1,22 @@
+import {useEffect, useState} from "react";
+import {useNavigate, useSearchParams} from "react-router-dom";
+//import fetch from "../classes/fetch.ts";
+import {useCookies} from "react-cookie";
+import Dashboard from "../subsites/Dashboard.tsx";
 
 function Main(): JSX.Element {
+    const [searchParams, ] = useSearchParams();
+    const [render, setRender] = useState<JSX.Element>()
+    const tool = searchParams.get("tool")
+
+    useEffect(() => {
+        switch (tool) {
+            default:
+                setRender(<Dashboard/>)
+                break
+        }
+    }, [tool])
+
     return (
         <>
             <section>
@@ -9,7 +26,7 @@ function Main(): JSX.Element {
                 <button>Add Tool</button>
             </section>
             <section>
-
+                {render}
             </section>
         </>
     );

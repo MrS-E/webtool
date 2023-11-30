4303b77dda7dfa780458e3e5d8a6365a11d529ef
MrS-E <simeon.stix@gmx.ch>
2023-11-30 21:03:18 +0100

    setup tests for password controller

--- webtool-be/src/passwords/passwords.controller.spec.ts
+++ webtool-be/src/passwords/passwords.controller.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordsController } from './passwords.controller';
+import {PasswordsService} from "./passwords.service";
+import {Password} from "@prisma/client";
 
 describe('PasswordsController', () => {
   let controller: PasswordsController;
+  let service: PasswordsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PasswordsController],
+      providers: [PasswordsService],
     }).compile();
 
+    service = module.get<PasswordsService>(PasswordsService);
     controller = module.get<PasswordsController>(PasswordsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('get Password(s)', () => {});//todo find out who to simulate a request
+  describe('create Password', () => {});
+  describe('update Password', () => {});
+  describe('delete Password', () => {});
 });

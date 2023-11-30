b6240133db349a1f44c83c3e7be228b553c868d7
MrS-E <simeon.stix@gmx.ch>
2023-11-30 20:37:58 +0100

    add user tests

--- webtool-be/src/user/user.controller.spec.ts
+++ webtool-be/src/user/user.controller.spec.ts
@@ -1,18 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import {UserService} from "./user.service";
+import {HttpStatus} from "@nestjs/common";
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
-
-    controller = module.get<UserController>(UserController);
+    service = new UserService();
+    controller = new UserController(service);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('Should return 201; everything in order', async () => {
+      const result : HttpStatus = HttpStatus.CREATED
+      jest.spyOn(service, 'createUser').mockImplementation(async () => result);
+
+      expect(await controller.createUser({
+        firstname: 'test',
+        lastname: 'test',
+        email: 'test@test.test',
+        auth: 'test'
+      })).toBe(result);
+    });
+
+    it('Should return 201; missing names', async () => {
+      const result : HttpStatus = HttpStatus.CREATED
+      jest.spyOn(service, 'createUser').mockImplementation(async () => result);
+
+      // @ts-ignore
+      expect(await controller.createUser({
+        email: 'test@test.test',
+        auth: 'test'
+      })).toBe(result);
+    });
+
+    it('Should return 500; missing auth', async () => {
+      const result: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+      jest.spyOn(service, 'createUser').mockImplementation(async () => result);//async for typescript
+
+      // @ts-ignore
+      expect(await controller.createUser({
+        firstname: 'test',
+        lastname: 'test',
+        email: 'test.test@test.test'
+      })).toBe(result);
+    });
+
+    it('Should return 500; missing mail', async () => {
+      const result: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+      jest.spyOn(service, 'createUser').mockImplementation(async () => result);
+
+      // @ts-ignore
+      expect(await controller.createUser({
+        firstname: 'test',
+        lastname: 'test',
+        auth: 'test'
+      })).toBe(result);
+    });
   });
-});
+})
+

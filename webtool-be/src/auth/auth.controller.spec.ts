e29f22b90b97f7d156bbafd353f68e2a1b1e2661
MrS-E <simeon.stix@gmx.ch>
2023-12-01 23:50:03 +0100

    add auth controller test

--- webtool-be/src/auth/auth.controller.spec.ts
+++ webtool-be/src/auth/auth.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import {HttpStatus} from "@nestjs/common";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "./constants";
+import {AuthService} from "./auth.service";
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +11,12 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService],
+      imports: [JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: { expiresIn: '60s' },
+      })]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +24,35 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('Create Token should return a token', async () => { //todo: fix this test
+    const result: string = ""
+    jest.spyOn(controller, 'createToken').mockImplementation(async () => result);
+    expect(await controller.createToken({email: "", password: ""})).toBe(result);
+  });
+  it('Create Token should throw UnauthorizedError', async () => {
+    const result : HttpStatus = HttpStatus.UNAUTHORIZED
+    //@ts-ignore
+    jest.spyOn(controller, 'createToken').mockImplementation(async () => result);
+    expect(await controller.createToken({email: "", password: ""})).toBe(result);
+  });
+  it('Create Token should throw SearchError', async () => {
+    const result : HttpStatus = HttpStatus.UNAUTHORIZED
+    //@ts-ignore
+    jest.spyOn(controller, 'createToken').mockImplementation(async () => result);
+    expect(await controller.createToken({email: "", password: ""})).toBe(result);
+  });
+  it('Create Token should throw InternalServerError', async () => {
+    const result : HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+    //@ts-ignore
+    jest.spyOn(controller, 'createToken').mockImplementation(async () => result);
+    expect(await controller.createToken({email: "", password: ""})).toBe(result);
+  });
+  it('Create Token missing argument should throw InternalServerError', async () => {
+    const result : HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+    //@ts-ignore
+    jest.spyOn(controller, 'createToken').mockImplementation(async () => result);
+    //@ts-ignore
+    expect(await controller.createToken({email: ""})).toBe(result);
   });
 });

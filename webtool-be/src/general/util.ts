efdb024be70f107b9841d4d1b18a4fb9ae5c008d
MrS-E <simeon.stix@gmx.ch>
2023-12-13 19:16:59 +0100

    update notes

--- webtool-be/src/general/util.ts
+++ webtool-be/src/general/util.ts
@@ -4,6 +4,7 @@ export async function trycatch(func: Function) :Promise<any> {
     try {
         return await func()
     } catch (e) {
+        console.error(e)
         throw new HttpException({
             status: e.status,
         }, e.status, {
--- webtool-ui/src/subsites/Notes.tsx
+++ webtool-ui/src/subsites/Notes.tsx
@@ -6,18 +6,20 @@ import Form from "../components/Form.tsx";
 import FormInput from "../components/FormInput.tsx";
 //@ts-ignore
 
-function Password() : JSX.Element {
+function Note() : JSX.Element {
     const navigate = useNavigate()
     const [cookies, ] = useCookies(["token"])
-    const [node, setNote] = useState<Object[]>([])
+    const [node, setNote] = useState<Object[]>()
     const [addTrigger, setAddTrigger] = useState<boolean>(false)
     const [add, setAdd] = useState({name:"", description:""})
     const [reload, setReload] = useState(0)
+    const [update, setUpdate] = useState<boolean>(false)
+    const [updateData, setUpdateData] = useState({id: "", update:{name:"", description:""}})
 
     useEffect(() => {
         fetch("http://localhost:3000/notes", {mode: "cors", method: "GET", headers: {"authorization": cookies.token}})
             .then(res =>res.status===401?navigate("/login"):res.json())
-            .then(res => setNote(res))
+            .then(res => setNote([...res]))
             .catch()
             .finally(()=>setReload(0))
 
@@ -31,6 +33,14 @@ function Password() : JSX.Element {
                 setAddTrigger(false)})
     }
 
+    const handleUpdate = () =>{
+        fetch("http://localhost:3000/notes/"+updateData.id, {mode: "cors", method:"PUT", headers: {"authorization": cookies.token, "Content-Type": "application/json"}, body: JSON.stringify(updateData.update)})
+            .then(res =>res.status===401?navigate("/login"):res.json())
+            .catch()
+            .finally(()=>{setReload(reload+1);
+                setUpdate(false)})
+    }
+
     return (
         <>
             <div>
@@ -41,14 +51,32 @@ function Password() : JSX.Element {
                     {node?.map((e:any, index:number) => {
                         return (
                             <div key={"node"+index+"_"+e.id}>
-                                <button onClick={()=>{
+                                <button style={update ? {display: "none"} : {}}
+                                    onClick={()=>{
                                     fetch("http://localhost:3000/notes/"+e.id, {mode: "cors", method: "DELETE", headers: {"authorization": cookies.token}})
                                         .finally(()=>{setReload(reload+1)})
                                 }}>Löschen</button>
-                                <button>Bearbeiten</button>{/*todo update*/}
-                                <div>
+                                <button style={update ? {display: "none"} : {}}
+                                        onClick={()=>{
+                                            setUpdateData({update:{name: e.name, description: e.description}, id: e.id})
+                                            setUpdate(true)
+                                        }}
+                                >Bearbeiten</button>{/*todo update*/}
+                                <div style={update ? {display: "none"} : {}}>
                                     <h4>{e.name}</h4>
                                     <p>{e.description}</p>
+                                </div>
+                                <div style={!update ? {display: "none"} : {}}>
+                                    <Form button={"Bearbeiten"} action={handleUpdate}>
+                                        <FormInput in={e.name} name={"name"} label={"Webseite"} type={"text"} required={true}
+                                                   value={(v: string) => {
+                                                       setUpdateData({...updateData, update: {...updateData.update, name: v}})
+                                                   }}/>
+                                        <FormInput in={e?.description} name={"user"} label={"Nutzername"} type={"text"}
+                                                   value={(v: string) => {
+                                                       setUpdateData({...updateData, update: {...updateData.update, description: v}})
+                                                   }}/>
+                                    </Form>
                                 </div>
                             </div>
                         )
@@ -59,7 +87,7 @@ function Password() : JSX.Element {
                 <h2>Hinzufügen</h2>
                 <hr/>
                 <Form button={"hinzufügen"} action={handleAdd}>
-                    <FormInput name={"name"} label={"Webseite"} type={"text"} required={true} value={(v:string)=>{setAdd({...add, name: v})}}/>
+                    <FormInput name={"name"} label={"Titel"} type={"text"} required={true} value={(v:string)=>{setAdd({...add, name: v})}}/>
                     <FormInput name={"note"} label={"Notiz"} type={"text"} value={(v:string)=>{setAdd({...add, description: v})}}/>
                 </Form>
             </Popup>
@@ -67,4 +95,4 @@ function Password() : JSX.Element {
     );
 }
 
-export default Password;
+export default Note;

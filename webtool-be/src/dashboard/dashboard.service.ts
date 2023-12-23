5870d34e50aade648a71e4bf6ca13f145247df7e
MrS-E <simeon.stix@gmx.ch>
2023-12-23 13:32:39 +0100

    update dashboard service tests

--- webtool-be/src/dashboard/dashboard.service.ts
+++ webtool-be/src/dashboard/dashboard.service.ts
@@ -1,23 +1,23 @@
 import {HttpStatus, Injectable} from '@nestjs/common';
 import {Note, Password, PrismaClient} from "@prisma/client";
+import {PrismaService} from "../prisma/prisma.service";
 
 @Injectable()
 export class DashboardService {
-    private readonly db: PrismaClient = new PrismaClient();
-
+    constructor( private prisma: PrismaService) {}
     getDashboard(userId:string) {
         return new Promise(async (resolve, reject) => {
-                /*this.db.user.findFirst({where: {id: userId}}) //todo implement dashboard customization
+                /*this.prisma.user.findFirst({where: {id: userId}}) //todo implement dashboard customization
                 .then((user)=>user.dashboard)
                 .then((dashboard)=>resolve(dashboard))
                 .catch((error)=>{console.log(error);reject({status: HttpStatus.INTERNAL_SERVER_ERROR, cause: error.message, error: error})})*/
             let dash  = {note: undefined, password: undefined};
-            dash.note = await this.db.note.findMany({where: {authorId: userId}, orderBy: {updatedAt: 'desc'}})
+            dash.note = await this.prisma.note.findMany({where: {authorId: userId}, orderBy: {updatedAt: 'desc'}})
                 .then((notes : Note[])=>notes.slice(0, notes.length>3?3:notes.length))
                 .then((notes : Note[])=>notes)
                 .catch((error)=>reject({status: HttpStatus.INTERNAL_SERVER_ERROR, cause: error.message, error: error}));
 
-            dash.password = await this.db.password.findMany({where: {authorId: userId}, orderBy: {updatedAt: 'desc'}, select:{name: true, email: true, username: true, description: true}})
+            dash.password = await this.prisma.password.findMany({where: {authorId: userId}, orderBy: {updatedAt: 'desc'}, select:{name: true, email: true, username: true, description: true}})
                 .then((passwords: Password[])=>passwords.slice(0, passwords.length>3?3:passwords.length))
                 .then((passwd: Password[])=>passwd)
                 .catch(error=>reject({status: HttpStatus.INTERNAL_SERVER_ERROR, cause: error.message, error: error}));

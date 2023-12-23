66f46a8a89a56f51f94253235f9018bc7f9b9000
MrS-E <simeon.stix@gmx.ch>
2023-12-23 12:30:58 +0100

    mini user controller and module updates

--- webtool-be/src/user/user.controller.spec.ts
+++ webtool-be/src/user/user.controller.spec.ts
@@ -1,14 +1,22 @@
 import { UserController } from './user.controller';
 import {UserService} from "./user.service";
 import {HttpStatus} from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import {PrismaService} from "../prisma/prisma.service";
 
 describe('UserController', () => {
   let controller: UserController;
   let service: UserService;
 
-  beforeEach(() => {
-    service = new UserService();
-    controller = new UserController(service);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService, PrismaService],
+    })
+        .compile();
+
+    service = module.get<UserService>(UserService);
+    controller = module.get<UserController>(UserController);
   });
 
   describe('create User', () => {

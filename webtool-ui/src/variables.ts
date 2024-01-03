82f7537617fee982bd78478e20e641710a3e17f3
MrS-E <simeon.stix@gmx.ch>
2024-01-03 12:17:16 +0100

    env for frontend

--- webtool-ui/src/variables.ts
+++ webtool-ui/src/variables.ts
@@ -1,1 +1,4 @@
-export const server = "http://localhost:3000"
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+export const server = process.env.SERVER?process.env.SERVER:"http://localhost:3000"

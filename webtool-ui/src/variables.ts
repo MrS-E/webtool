77fe1b2ff1ca6b67b423ee154b32ffdaa9d3a063
MrS-E <simeon.stix@gmx.ch>
2024-01-06 00:34:59 +0100

    small changes to fix errors

--- webtool-ui/src/variables.ts
+++ webtool-ui/src/variables.ts
@@ -1,4 +1,1 @@
-import * as dotenv from 'dotenv'
-
-dotenv.config();
-export const server = process.env.SERVER?process.env.SERVER:"http://localhost:3000"
+export const server = "http://localhost:3000"
